68e9f1af5cca6af16bb7eb3bd34db47cf87d5618
sonatuzun <sonatuzun@yahoo.com.tr>
2026-02-28 03:19:53 +0300

    added top and bottom border collisions

--- Assets/Photon/Pong/Simulation/BallSystem.cs
+++ Assets/Photon/Pong/Simulation/BallSystem.cs
@@ -1,60 +1,36 @@
 namespace Quantum.Pong
 {
-  using Photon.Deterministic;
-
-  /// <summary>
-  /// The <c>AsteroidsProjectileSystem</c> class manages the lifecycle of projectiles,
-  /// including updating their time-to-live (TTL) and handling projectile shooting signals.
-  /// </summary>
-  public unsafe class BallSystem : SystemMainThreadFilter<BallSystem.Filter>
-  {
-    /// <summary>
-    /// The <c>Filter</c> struct represents the components required for the system's operations,
-    /// including an entity reference and a pointer to its projectile component.
-    /// </summary>
-    public struct Filter
-    {
-      public EntityRef Entity;
-      public Transform2D* Transform;
-      public Ball* Ball;
-    }
+    using Photon.Deterministic;
 
     /// <summary>
-    /// Updates TTL of projectiles and destroying them if TTL reaches zero.
+    /// The <c>AsteroidsProjectileSystem</c> class manages the lifecycle of projectiles,
+    /// including updating their time-to-live (TTL) and handling projectile shooting signals.
     /// </summary>
-    /// <param name="f">The game frame.</param>
-    /// <param name="filter">The filter containing the entity and its projectile component.</param>
-    public override void Update(Frame f, ref Filter filter)
+    public unsafe class BallSystem : SystemMainThreadFilter<BallSystem.Filter>
     {
-        filter.Transform->Position += filter.Ball->Velocity;
+        public struct Filter
+        {
+            public EntityRef Entity;
+            public Transform2D* Transform;
+            public Ball* Ball;
+        }
+
+        public override void Update(Frame f, ref Filter filter)
+        {
+            filter.Transform->Position += filter.Ball->Velocity;
+
+            HandleBorderCollisions(f, ref filter);
+        }
+
+        public void HandleBorderCollisions(Frame f, ref Filter filter)
+        {
+            PongGameConfig config = f.FindAsset(f.RuntimeConfig.GameConfig);
+            FP extends = config.GameMapSize.Y / 2;
+            
+            if (FPMath.Abs(filter.Transform->Position.Y) > extends)
+            {
+                filter.Ball->Velocity.Y *= -1;
+            }
+        }
     }
-
-    /// <summary>
-    /// Handles the shooting of a projectile by a ship. This method creates a new projectile,
-    /// sets its initial position and velocity, and assigns it to the shooting ship.
-    /// </summary>
-    /// <param name="f">The game frame.</param>
-    /// <param name="owner">The reference to the entity (ship) that is shooting the projectile.</param>
-    /// 
-    /*
-    public void AsteroidsShipShoot(Frame f, EntityRef owner)
-    {
-      AsteroidsGameConfig config = f.FindAsset(f.RuntimeConfig.GameConfig);
-      EntityRef projectileEntity = f.Create(config.ProjectilePrototype);
-      Transform2D* projectileTransform = f.Unsafe.GetPointer<Transform2D>(projectileEntity);
-      Transform2D* ownerTransform = f.Unsafe.GetPointer<Transform2D>(owner);
-
-      var relativeOffset = FPVector2.Up * config.ShotOffset;
-      projectileTransform->Rotation = ownerTransform->Rotation;
-      projectileTransform->Position = ownerTransform->TransformPoint(relativeOffset);
-      
-      AsteroidsProjectile* projectile = f.Unsafe.GetPointer<AsteroidsProjectile>(projectileEntity);
-      projectile->TTL = config.ProjectileTTL;
-      projectile->Owner = owner;
-
-      PhysicsBody2D* body = f.Unsafe.GetPointer<PhysicsBody2D>(projectileEntity);
-      body->Velocity = ownerTransform->Up * config.ProjectileInitialSpeed;
-    }
-    */
-  }
 }
--- Assets/Photon/Pong/Simulation/CollisionsSystem.cs
+++ Assets/Photon/Pong/Simulation/CollisionsSystem.cs
@@ -2,19 +2,8 @@ using Photon.Deterministic;
 
 namespace Quantum.Pong
 {
-    public unsafe class CollisionsSystem : SystemSignalsOnly, ISignalOnCollisionEnter2D, ISignalOnTriggerEnter2D
+    public unsafe class CollisionsSystem : SystemSignalsOnly, ISignalOnTriggerEnter2D
     {
-        public void OnCollisionEnter2D(Frame f, CollisionInfo2D info)
-        {
-            if (f.Unsafe.TryGetPointer<Ball>(info.Entity, out var ball))
-            {
-                if (f.Has<Paddle>(info.Other))
-                {
-                    HandleBallHitPaddle(f, ball);
-                }
-            }
-        }
-
         public void OnTriggerEnter2D(Frame f, TriggerInfo2D info)
         {
             if (f.Unsafe.TryGetPointer<Paddle>(info.Entity, out var paddle))
