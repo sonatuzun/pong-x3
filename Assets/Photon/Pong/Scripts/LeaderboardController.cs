b669085cef28daed04fd9db19d091b2428d73d1a
sonatuzun <sonatuzun@yahoo.com.tr>
2026-03-03 18:20:29 +0300

    create script and prefabs, plan for future

---
+++ Assets/Photon/Pong/Scripts/LeaderboardController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class LeaderboardController : MonoBehaviour
+{
+    // get data from a file
+    // get entries object 
+    // populate popup
+    // be able to close the popup
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
